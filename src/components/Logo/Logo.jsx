518b4a943f10bcdb710cb652b372597b3f11388e
MdRehanAli <rehanali21d@gmail.com>
2025-11-19 19:57:50 +0600

    Added navbar footer and banner

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../../assets/logo.png'
+
+const Logo = () => {
+    return (
+        <div className='w-[150px]'>
+            <img src={logo} alt="Logo" />
+        </div>
+    );
+};
+
+export default Logo;
