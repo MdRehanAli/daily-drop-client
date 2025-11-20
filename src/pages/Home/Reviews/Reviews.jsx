f74c851bd9ed0d7c2f5e5365302349950fd5144b
MdRehanAli <rehanali21d@gmail.com>
2025-11-20 22:11:50 +0600

    Added Reviews

---
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -0,0 +1,15 @@
+import React, { use } from 'react';
+
+const Reviews = ({ reviewsPromise }) => {
+
+    const reviews = use(reviewsPromise);
+    console.log(reviews);
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Reviews;
