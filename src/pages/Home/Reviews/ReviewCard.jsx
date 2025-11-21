e7d193cfdd5e28c445896844ce40ad3052f26c8c
MdRehanAli <rehanali21d@gmail.com>
2025-11-21 19:59:21 +0600

    Added Reviews card

---
+++ src/pages/Home/Reviews/ReviewCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+
+const ReviewCard = ({ review }) => {
+    const { userName, review: testimonial, user_photoURL, user_email } = review;
+    return (
+        <div className="max-w-lg mx-auto">
+            <div className="bg-white rounded-2xl shadow-lg border border-gray-100 p-8 relative">
+                {/* quote icon */}
+                <div className="absolute top-6 left-6">
+                    <div className="w-10 h-10 rounded-lg flex items-center justify-center bg-teal-50 text-teal-300">
+                        <FaQuoteRight className="text-2xl" />
+                    </div>
+                </div>
+
+                {/* content */}
+                <div className="pt-6">
+                    <p className="text-gray-700 leading-relaxed text-base md:text-lg font-medium">
+                        {testimonial}
+                    </p>
+
+                    {/* dashed divider */}
+                    <div className="mt-6">
+                        <div className="w-full border-t-2 border-dashed border-teal-200" />
+                    </div>
+
+                    {/* footer */}
+                    <div className="flex items-center gap-4 mt-6">
+                        {/* colored circle avatar */}
+                        <div className="w-12 h-12 rounded-full bg-teal-900 shrink-0">
+                            <img src={user_photoURL} alt="" />
+                        </div>
+                        <div>
+                            <h3 className="text-teal-900 font-extrabold text-lg md:text-xl leading-tight">
+                                {userName}
+                            </h3>
+                            <p className="text-gray-400 text-sm md:text-base">
+                                {user_email}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewCard;
--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -1,4 +1,7 @@
 import React, { use } from 'react';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import ReviewCard from './ReviewCard';
 
 const Reviews = ({ reviewsPromise }) => {
 
@@ -6,8 +9,35 @@ const Reviews = ({ reviewsPromise }) => {
     console.log(reviews);
 
     return (
-        <div>
-
+        <div className='my-24'>
+            <div className='text-center'>
+                <h3 className='text-3xl font-bold text-center my-8'>Review</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius accusantium saepe sint esse dolore enim nam, quaerat id quis, voluptas, placeat illo quidem! Culpa soluta dolore voluptatem velit aperiam, eius odio fugit nam, voluptatibus cupiditate vero mollitia. Pariatur delectus cumque culpa illum voluptatibus, vero ipsam numquam suscipit sapiente, nesciunt modi.</p>
+            </div>
+            <>
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={3}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper"
+                >
+                    {
+                        reviews.map(review => <SwiperSlide key={review.id}>
+                            <ReviewCard review={review}></ReviewCard>
+                        </SwiperSlide>)
+                    }
+                </Swiper>
+            </>
         </div>
     );
 };
