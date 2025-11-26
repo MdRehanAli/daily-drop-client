8e4cea0cc9790508ffd31efc8f99db3bf56fe78d
MdRehanAli <rehanali21d@gmail.com>
2025-11-26 17:20:46 +0600

    Added card

--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -6,7 +6,6 @@ import ReviewCard from './ReviewCard';
 const Reviews = ({ reviewsPromise }) => {
 
     const reviews = use(reviewsPromise);
-    // console.log(reviews);
 
     return (
         <div className='my-24'>
