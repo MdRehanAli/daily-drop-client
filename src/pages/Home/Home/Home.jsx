3adb85ec3c599a3e973efe3ffbbeeae3a57d9015
MdRehanAli <rehanali21d@gmail.com>
2025-11-29 20:46:03 +0600

    Added Process Section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,10 +4,12 @@ import Brands from '../Brands/Brands';
 import Reviews from '../Reviews/Reviews';
 import Works from '../Works/Works';
 import Services from '../Services/Services';
+import Process from '../Process/Process';
 
 const reviewsPromise = fetch('/reviews.json').then(res => res.json())
 const worksPromise = fetch('/works.json').then(res => res.json());
 const servicesPromise = fetch('/services.json').then(res => res.json())
+const processPromise = fetch('/process.json').then(res => res.json())
 
 const Home = () => {
     return (
@@ -16,6 +18,7 @@ const Home = () => {
             <Works worksPromise={worksPromise}></Works>
             <Services servicesPromise={servicesPromise}></Services>
             <Brands></Brands>
+            <Process processPromise={processPromise}></Process>
             <Reviews reviewsPromise={reviewsPromise}></Reviews>
         </div>
     );
---
+++ src/pages/Home/Process/Process.jsx
@@ -0,0 +1,24 @@
+import React, { use } from 'react';
+import parcel from '../../../assets/live-tracking.png'
+// import delivery from '../../../assets/safe-delivery.png'
+
+const Process = ({ processPromise }) => {
+
+    const processes = use(processPromise);
+
+    return (
+        <div className='flex flex-col gap-6 my-[100px]'>
+            {
+                processes.map(process => <div key={process.id} className='flex items-center p-8 gap-12 bg-white rounded-3xl'>
+                    <img src={parcel} alt="" />
+                    <div className='border-dashed  border-l-2 pl-12 py-7 border-[#03464D]'>
+                        <p className='text-secondary text-2xl font-extrabold mb-4'>{process.title}</p>
+                        <p className='text-[#606060]'>{process.description}</p>
+                    </div>
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default Process;
