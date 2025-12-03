2ded4d1c433792da59d951086843ece28ffee075
MdRehanAli <rehanali21d@gmail.com>
2025-12-03 06:50:21 +0600

    Added About

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import logo from '../../assets/logo.png'
+import { Link } from 'react-router';
 
 const Logo = () => {
     return (
-        <div className='w-[150px]'>
-            <img src={logo} alt="Logo" />
-        </div>
+        <Link to='/'>
+            <div className='w-[150px]'>
+                <img src={logo} alt="Logo" />
+            </div>
+        </Link>
     );
 };
 
