90d00d84c8b20bac62914071ce5e225463691d99
MdRehanAli <rehanali21d@gmail.com>
2025-12-03 06:37:36 +0600

    Added Leaflet

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
 import { useLoaderData } from 'react-router';
@@ -9,14 +9,51 @@ const Coverage = () => {
     const serviceCenters = useLoaderData();
     console.log(serviceCenters);
 
+    const mapRef = useRef(null)
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const location = e.target.location.value;
+        const district = serviceCenters.find(c => c.district.toLowerCase().includes(location.toLowerCase()));
+
+        if (district) {
+            const coord = [district.latitude, district.longitude];
+            console.log(district, coord);
+
+            // GO to the location through Ref and using LeafletJs 
+            mapRef.current.flyTo(coord, 14);
+        }
+    }
+
     return (
         <div className='max-w-7xl mx-auto w-11/12 mt-8 py-20 px-[100px] bg-white rounded-4xl'>
             <h1 className='text-4xl font-extrabold text-secondary'>We are available in 64 districts</h1>
             <div>
-                {/* ** */}
+                <form onSubmit={handleSearch}>
+                    <label className="input">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input type="search" className="grow" placeholder="Search" name='location' />
+
+                    </label>
+                </form>
             </div>
             <div>
-                <MapContainer center={position} zoom={8} scrollWheelZoom={false}
+                <MapContainer
+                    center={position}
+                    zoom={8}
+                    scrollWheelZoom={false}
+                    ref={mapRef}
                     className='h-[600px] w-full'>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
