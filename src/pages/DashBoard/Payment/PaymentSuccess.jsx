9f3ef9b180a6eb491deb662ada3a662d9c692585
MdRehanAli <rehanali21d@gmail.com>
2025-12-15 21:18:55 +0600

    Added parcels payment

--- src/pages/DashBoard/Payment/PaymentSuccess.jsx
+++ src/pages/DashBoard/Payment/PaymentSuccess.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const PaymentSuccess = () => {
     const [searchParams] = useSearchParams();
+    const [paymentInfo, setPaymentInfo] = useState({})
     const sessionId = searchParams.get('session_id');
     const axiosSecure = useAxiosSecure();
 
@@ -14,6 +15,10 @@ const PaymentSuccess = () => {
             axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
                 .then(res => {
                     console.log(res.data);
+                    setPaymentInfo({
+                        trackingId: res.data.trackingId,
+                        transactionId: res.data.trackingId
+                    })
                 })
         }
     }, [axiosSecure, sessionId])
@@ -21,6 +26,8 @@ const PaymentSuccess = () => {
     return (
         <div>
             <h2 className='text-5xl'>Payment successful</h2>
+            <p>Your Transaction Id is: {paymentInfo.transactionId}</p>
+            <p>Your Tracking Id is: {paymentInfo.trackingId}</p>
         </div>
     );
 };
