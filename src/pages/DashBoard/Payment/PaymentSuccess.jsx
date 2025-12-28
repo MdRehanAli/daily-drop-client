f80ac72b7bfc4cc22c703afa6801808abd3f39ba
MdRehanAli <rehanali21d@gmail.com>
2025-12-28 23:49:43 +0600

    Added payment success layout

--- src/pages/DashBoard/Payment/PaymentSuccess.jsx
+++ src/pages/DashBoard/Payment/PaymentSuccess.jsx
@@ -24,10 +24,10 @@ const PaymentSuccess = () => {
     }, [axiosSecure, sessionId])
 
     return (
-        <div>
-            <h2 className='text-5xl'>Payment successful</h2>
-            <p>Your Transaction Id is: {paymentInfo.transactionId}</p>
-            <p>Your Tracking Id is: {paymentInfo.trackingId}</p>
+        <div className='shadow-xl bg-gray-100 rounded-2xl p-5 py-10 md:p-10 w-11/12 mx-auto mt-8'>
+            <h1 className='text-3xl md:text-5xl text-center font-extrabold text-secondary mb-8'>Payment successful</h1>
+            <p className='text-center text-xl'>Your Transaction Id is: {paymentInfo.transactionId}</p>
+            <p className='text-center text-xl'>Your Tracking Id is: {paymentInfo.trackingId}</p>
         </div>
     );
 };
