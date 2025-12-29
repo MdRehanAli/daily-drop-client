e9db014b81bad0b97886779d2e6157a1991dcfa2
MdRehanAli <rehanali21d@gmail.com>
2025-12-29 18:42:25 +0600

    Added payment Cancel layout

--- src/pages/DashBoard/Payment/PaymentCancelled.jsx
+++ src/pages/DashBoard/Payment/PaymentCancelled.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router';
 
 const PaymentCancelled = () => {
     return (
-        <div>
-            <h1>Payment is Cancelled. Please try again.</h1>
+        <div className='shadow-xl bg-gray-100 rounded-2xl p-5 py-10 md:p-10 w-11/12 mx-auto mt-8'>
+            <h1 className='text-3xl md:text-5xl text-center font-extrabold text-secondary mb-8'>Payment is Cancelled. Please try again.</h1>
             <Link to='/dashboard/my-parcels' className='btn btn-primary text-black'>Try Again</Link>
         </div>
     );
