a2e66f4f859f78c3c293e519df4193f794921f7d
MdRehanAli <rehanali21d@gmail.com>
2025-12-30 12:02:20 +0600

    Added payment layout update

--- src/pages/DashBoard/Payment/Payment.jsx
+++ src/pages/DashBoard/Payment/Payment.jsx
@@ -17,7 +17,7 @@ const Payment = () => {
         }
     })
 
-    const handlePayment = async() => {
+    const handlePayment = async () => {
         const paymentInfo = {
             cost: parcel.cost,
             parcelId: parcel._id,
@@ -36,8 +36,8 @@ const Payment = () => {
     }
 
     return (
-        <div>
-            <h1>Please Pay ${parcel.cost} for {parcel.parcelName}</h1>
+        <div className='shadow-xl bg-gray-100 rounded-2xl p-5 py-10 md:p-10 w-11/12 mx-auto mt-8'>
+            <h1 className='text-3xl md:text-5xl text-center font-extrabold text-secondary mb-8'>Please Pay ${parcel.cost} for {parcel.parcelName}</h1>
             <button onClick={handlePayment} className='btn btn-primary text-black'>Pay</button>
         </div>
     );
